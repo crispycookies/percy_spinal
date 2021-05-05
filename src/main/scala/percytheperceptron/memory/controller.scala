d90fc8b9ea94c4b4bc1b506b1be1be1d61fdf5e6
crispycookies <crispycookies@users.noreply.github.com>
2021-05-06 00:28:30 +0200

    Fixed Regfile

--- src/main/scala/percytheperceptron/memory/controller.scala
+++ src/main/scala/percytheperceptron/memory/controller.scala
@@ -15,23 +15,33 @@ class controller(address_bit_width : Int, bit_width : Int, row_count : Int, sub_
     val read = out Vec(UInt(bit_width bits), sub_cell_count)
     val store = in Vec(UInt(bit_width bits), sub_cell_count)
   }
-  val address = UInt(log2Up(row_count) bits)
-
-  address := io.address(log2Up(row_count)-1 downto 0)
+  //val address = UInt(log2Up(row_count) bits)
+  //address := io.address(log2Up(row_count)-1 downto 0)
 
   io.write_ena.map(_ := 0)
   io.read.map(_ := 0)
 
   for (i <- io.features_to_store) {
-    i.map(_ := 0)
+    i := io.store
   }
 
-  when(address <= row_count){
-    io.features_to_store(address) := io.store
-    io.read := io.features_from_store(address)
+  when(io.address <= row_count){
     when(io.write_ena_user === 1) {
-      io.write_ena(address) := 1
+      io.write_ena(io.address) := 1
     }
   }
+  when(io.address <= row_count){
+    when(io.write_ena_user === 0) {
+      io.read := io.features_from_store(io.address)
+    }
+  }
+
+  /*when(io.address <= row_count){
+    io.features_to_store(io.address) := io.store
+    io.read := io.features_from_store(io.address)
+    when(io.write_ena_user === 1) {
+      io.write_ena(io.address) := 1
+    }
+  }*/
 }
 
--- src/main/scala/percytheperceptron/percy.scala
+++ src/main/scala/percytheperceptron/percy.scala
@@ -14,6 +14,6 @@ class percy(test: Int) extends Component {
 // This is the main function that generates the VHDL and the Verilog corresponding to MyTopLevel.
 object PercyMain {
   def main(args: Array[String]) {
-    SpinalVerilog(new controller(bit_width = 16, sub_cell_count = 2, row_count = 2, address_bit_width = 3))
+    SpinalVerilog(new register_file(bit_width = 16, sub_cell_count = 2, row_count = 2, address_bit_width = 1))
   }
 }
