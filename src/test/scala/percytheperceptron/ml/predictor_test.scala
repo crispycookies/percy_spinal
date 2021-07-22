57964ff3dc7b50f6e66b840388705b3d2d4482d7
tobi <egtobi@gmail.com>
2021-07-23 01:15:44 +0200

    ?=???

--- src/test/scala/percytheperceptron/ml/predictor_test.scala
+++ src/test/scala/percytheperceptron/ml/predictor_test.scala
@@ -67,5 +67,128 @@ object PredictorTests {
       assert(dut.io.history(0).toInt == 1)
       */
     }
+    val core2 = SimConfig.withWave.compile(new predictor(bit_width = 16, feature_count = 3, table_size = 2, address_bit_width = 16, index_bit_width = 16, -1,1,0,3))
+    core2.doSim("Check Wiring"){ dut =>
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 1
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+
+      /*assert(dut.io.history(2).toInt == 0)
+      assert(dut.io.history(1).toInt == 1)
+      assert(dut.io.history(0).toInt == 1)
+      */
+    }
+    val core3 = SimConfig.withWave.compile(new predictor(bit_width = 16, feature_count = 3, table_size = 2, address_bit_width = 16, index_bit_width = 16, 0,1,0,1))
+    core3.doSim("Check Wiring"){ dut =>
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 1
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+
+      /*assert(dut.io.history(2).toInt == 0)
+      assert(dut.io.history(1).toInt == 1)
+      assert(dut.io.history(0).toInt == 1)
+      */
+    }
+
   }
 }
