9aa309ad48cbeea99d3d14750faa71af6ed1c5de
tobi <egtobi@gmail.com>
2021-07-23 01:17:29 +0200

    Expanded Perceptron

--- src/main/scala/percytheperceptron/ml/perceptron/perceptron.scala
+++ src/main/scala/percytheperceptron/ml/perceptron/perceptron.scala
@@ -9,6 +9,7 @@ class perceptron(bit_width: Int, feature_count: Int, lower_bound: Int, upper_bou
     val weights: Vec[SInt] = in Vec(SInt(bit_width bits), feature_count)
     val values: Vec[SInt] = in Vec(SInt(bit_width bits), feature_count)
     val prediction: SInt = out SInt(bit_width bits)
+    val prediction_numeric : SInt = out SInt(bit_width bits)
   }
   val percy = new perceptron_core(bit_width = bit_width, feature_count = feature_count)
   val activation_function = new activation(bit_width = bit_width, lower_bound = lower_bound, upper_bound = upper_bound, zero = zero);
@@ -19,5 +20,6 @@ class perceptron(bit_width: Int, feature_count: Int, lower_bound: Int, upper_bou
 
   activation_function.io.trigger := percy.io.out_data
   io.prediction := activation_function.io.step
+  io.prediction_numeric := percy.io.out_data
 }
 
