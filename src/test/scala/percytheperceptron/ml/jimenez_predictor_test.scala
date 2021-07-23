3c207595f7173dd693354ef2a23ec763172d6a27
tobi <egtobi@gmail.com>
2021-07-23 17:26:12 +0200

    Added Jimenez Predictor Test

--- src/test/scala/percytheperceptron/ml/jimenez_predictor_test.scala
+++ src/test/scala/percytheperceptron/ml/jimenez_predictor_test.scala
@@ -1,13 +1,76 @@
 package percytheperceptron.ml
 
-import percytheperceptron.predictor
+import percytheperceptron.{predictor, predictor_jimenez}
 import spinal.sim._
 import spinal.core.sim._
 
 object JimenezPredictorTests {
   def main(args: Array[String]): Unit = {
-    val core = SimConfig.withWave.compile(new predictor(bit_width = 16, feature_count = 3, table_size = 2, address_bit_width = 16, index_bit_width = 16, -1,1,0,1))
+    val core = SimConfig.withWave.compile(new predictor_jimenez(bit_width = 16, feature_count = 3, table_size = 2, address_bit_width = 16, index_bit_width = 16, -1,1,0,1, 127))
     core.doSim("Check Wiring"){ dut =>
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 0
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 1
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
       dut.clockDomain.forkStimulus(10)
       dut.io.address #= 0
       dut.io.taken #= 1
@@ -31,20 +94,73 @@ object JimenezPredictorTests {
       dut.io.taken #= 1
       dut.clockDomain.waitSampling()
       dut.clockDomain.waitSampling()
-      dut.io.taken #= 0
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
       dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
       dut.clockDomain.waitSampling()
       dut.io.taken #= 1
       dut.clockDomain.waitSampling()
       dut.clockDomain.waitSampling()
-      dut.io.address #= 1
-      dut.io.taken #= 0
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
       dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
       dut.clockDomain.waitSampling()
       dut.io.taken #= 1
       dut.clockDomain.waitSampling()
       dut.clockDomain.waitSampling()
+      dut.clockDomain.forkStimulus(10)
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
       dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 0
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      dut.io.address #= 0
+      dut.io.taken #= 1
+      println(dut.io.prediction.toInt)
+      dut.clockDomain.waitSampling()
+      dut.io.taken #= 1
       dut.clockDomain.waitSampling()
       dut.clockDomain.waitSampling()
       dut.clockDomain.waitSampling()
@@ -67,7 +183,7 @@ object JimenezPredictorTests {
       assert(dut.io.history(0).toInt == 1)
       */
     }
-    val core2 = SimConfig.withWave.compile(new predictor(bit_width = 16, feature_count = 3, table_size = 2, address_bit_width = 16, index_bit_width = 16, -1,1,0,3))
+    val core2 = SimConfig.withWave.compile(new predictor_jimenez(bit_width = 16, feature_count = 3, table_size = 2, address_bit_width = 16, index_bit_width = 16, -1,1,0,3, 127))
     core2.doSim("Check Wiring"){ dut =>
       dut.clockDomain.forkStimulus(10)
       dut.io.address #= 0
